00816387ef7f338058761e574496d289fd39b0d2
Martin Frichitthavong <frichitthavongmartin@gmail.com>
2020-05-23 21:05:30 -0400

    asf

--- Graphics/Form1.cs
+++ Graphics/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-namespace Graphics
+namespace Graphics// Graphics - Martin Frichitthavong 5/23/2020
 {
     public partial class Form1 : Form
     {
@@ -18,10 +18,6 @@ namespace Graphics
         public Form1()
         {
             InitializeComponent();
-            
-          
-            
-           
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,15 +28,19 @@ namespace Graphics
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             System.Drawing.Graphics g;
-
             g = this.CreateGraphics();
+
             Pen drawPen = new Pen(Color.Black, 29);
             SolidBrush drawBrush = new SolidBrush(Color.Red);
+
             drawPen.Color = Color.White;
             drawBrush.Color = Color.Red;
+
             g.FillEllipse(drawBrush, 125, 75, 165, 165);
             g.DrawEllipse(drawPen, 163, 110, 90, 90);
+
             Font drawFont = new Font("Arial", 32, FontStyle.Bold);
+
             g.DrawString("TARGET", drawFont, drawBrush, 120, 240);
 
         }
